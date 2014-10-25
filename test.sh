4a08c77e016c7f870ddb547c0625fd8760ed028c
Megax <megax@yeahunter.hu>
2014-10-25 14:32:15 +0200

    * test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-xbuild /p:Configuration="Debug" Wunderground.Net.sln /flp:LogFile=xbuild.log;Verbosity=Detailed
+xbuild /p:Configuration="Debug" nunit.Schumix.sln /flp:LogFile=xbuild.log;Verbosity=Detailed
 
 #cd Run/Tests/Debug
 #nunit-console Test.dll
